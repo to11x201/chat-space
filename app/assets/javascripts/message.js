3be67bcd7aaa4d68d49ae946ea25aae110b13eb3
to11x201 <66550986+to11x201@users.noreply.github.com>
2020-07-26 20:03:47 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,24 +37,7 @@ $(function(){
       </div>
     </div>`
       return html;
-    } else {
-      var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
+    
     };
   }
   $('#new_message').on('submit', function(e){
